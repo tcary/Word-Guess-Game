e53eaf511d4e35aad42c5106dc212fbca5e40492
Tetiana Cary <taniushka.tata@gmail.com>
2019-09-07 12:21:15 -0600

    Replaced images with gifs,updated index.html with new font style and added divs. Updated functions in game.js and changed vars.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,137 @@
-// // Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
-// Use key events to listen for the letters that your players will type.
-// Display the following on the page:
-// Press any key to get started!
-// Wins: (# of times user guessed the word correctly).
 
+// words list
+var words = ["TIGER", "PANDA", "ELEPHANT", "MONKEY", "ZEBRA", "GIRAFFE", "LION", "HYENA"];
 
+var maxNumGuesses = 8; // max number of guesses 
+var guessedLetters = []; // store the guessed letters
+var ansWordArr = []; // store the "_" and to be used to replace the word answer
+var numGuessesRemaining = 0; // number of guesses remaining
+var numWins = 0; // number of wins
+var numLosses = 0; // number of losses
+var isFinished = false; // when true, game can start again
+var ansWord; // the word that is being played
 
-// If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
-// As the user guesses the correct letters, reveal them: m a d o _  _ a.
+// function runs at the start of page and used to restart after game isFinished
+function setup() {
+    //picks random word from words list
+    ansWord = words[Math.floor(Math.random() * words.length)];
 
+    ansWordArr = [];
 
-
-// Number of Guesses Remaining: (# of guesses remaining for the user).
-// Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
-// After the user wins / loses the game should automatically choose another word and make the user play it.
-// compare user guess to the chosen word from animal list and loop over it to compare
-// keep track if wrong take away guessLeft, if wright print it on the screen to html
-// if all guessed correctly user won show pic of an animal, if lost start again
-// set arrays for each animal and if the letter is guessed correctly display in an open slot for it.
-//once the word is guessed show the picture corresponding to the animal
-//starts the screen -reset the game-pick an animal-check the user guess against thetries left. create a var of correct guesses if the same that with current
-//global variables, functions, events
-var wins = 0;
-var losses = 0;
-var userGuess = [];
-var guessLeft = 15;
-var animalList = ["elephant", "monkey", "tiger", "panda", "zebra"];
-var computerGuess;
-var correctGuesses = [];
-
-// vars for quick reference to html
-var directionsText = document.getElementById("directions");
-var userChoiceText = document.getElementById("user-choice");
-var winsText = document.getElementById("wins");
-var lossesText = document.getElementById("losses");
-
-function resetGame() {
-    //display a pic of correct animal guessed
-    guessLeft = 15;
-    correctGuesses = [];
-    computerGuess = animalList[Math.floor(Math.random() * animalList.length)].split("");
-    for (var i = 0; i < computerGuess.length; i++) {
-        correctGuesses.push("_");
+    // adds "_" to ansWordArr
+    for (var i = 0; i < ansWord.length; i++) {
+        ansWordArr[i] = "_";
     }
-    console.log(correctGuesses);
-    console.log(computerGuess);
-}
-// function checkIn() {
-//     if (userGuess === animal[i]) {
-//         console.log(animal)
-//     }
-// }
 
-for (var i = 0; i < animal.length; i++) {
-    console.log(animal[i])
-}
+    // reset the variables 
+    numGuessesRemaining = maxNumGuesses;
+    guessedLetters = [];
+
+    //clears giphy-embed to now show any gifs
+    document.getElementById("giphy-embed").src = "";
+    //removes color from numGuesses
+    document.getElementById("numGuesses").style.color = "";
+
+    //show the selected elements on the screen 
+    updateScreen();
+};
+
+//updates the HTML from the functions
+function updateScreen() {
+    document.getElementById("numWins").innerText = numWins;
+    document.getElementById("numLosses").innerText = numLosses;
+    document.getElementById("numGuesses").innerText = numGuessesRemaining;
+    document.getElementById("answerWord").innerText = ansWordArr.join("");
+    document.getElementById("guessedLetters").innerText = guessedLetters;
+
+};
+
+//function to check the key that's pressed
+function checkGuess(letter) {
+    //if letter is not in guessedLetters array then push the letter to the array
+    if (guessedLetters.indexOf(letter) === -1) {
+        guessedLetters.push(letter);
+        //if the letter isn't in the answer word then -1 the numGuessesRemaining
+        if (ansWord.indexOf(letter) === -1) {
+            numGuessesRemaining--;
+            //if numGuessesRemaining is 3 or less then change the color
+            if (numGuessesRemaining <= 3) {
+                document.getElementById("numGuesses").style.color = "#e12d2e";
+            }
+            //if letter is in answer then replace the positioned "_" with the letter
+        } else {
+            for (var i = 0; i < ansWord.length; i++) {
+                if (letter === ansWord[i]) {
+                    ansWordArr[i] = letter;
+                }
+            }
+        }
+    }
+
+};
+
+//function to check if the player is a winner
+function isWinner() {
+    //if there are no more "_" in the ansWordArr then +1 to Wins and switch isFinished to true
+    if (ansWordArr.indexOf("_") === -1) {
+        numWins++;
+        isFinished = true;
+        //if the answer is guessed then play assigned gif
+        if (ansWord === "TIGER") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/TFTJhIlix4cxVsMVye";
+        } else if (ansWord === "PANDA") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/wT71Ce9oKBQGc";
+        } else if (ansWord === "ELEPHANT") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/JZn9hkPaRhcmQ";
+        } else if (ansWord === "MONKEY") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/l1KVboXQeiaX7FHgI";
+        } else if (ansWord === "ZEBRA") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/lJUCkbSDUuGWPKaiBM";
+        } else if (ansWord === "GIRAFFE") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/ZkiIapyGO0u6Q";
+        } else if (ansWord === "LION") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/26vIftJr40wzGUadO";
+        } else if (ansWord === "HYENA") {
+            document.getElementById("giphy-embed").src = "https://giphy.com/embed/E0efohBrnFk3Q41Sjj";
+        }
+
+    }
+};
+//function to check if player is a loser
+function isLoser() {
+    // if the numGuessesRemaining is 0 then -1 numLosses and switch isFinished to true
+    if (numGuessesRemaining <= 0) {
+        numLosses++;
+        isFinished = true;
+        //play the loser gif
+        document.getElementById("giphy-embed").src = "https://giphy.com/embed/1d5I0OuQ4vyDMnkY3Q";
+        document.getElementById("numLosses").style.color = "#e12d2e";
+    }
+
+};
 
 
+//event listener for key pressed
 document.onkeyup = function (event) {
-    if (wins === 0 && losses === 0 && userGuess === 0) {
-        resetGame();
+    //if isFinished is true then restart the game to the initial setup 
+    //and switch isFinished back to false
+    if (isFinished) {
+        setup();
+        isFinished = false;
+    } else {
+        //check to see if only letters A-Z are pressed
+        //functions are executed when user presses A-Z key
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            checkGuess(event.key.toUpperCase());
+            updateScreen();
+            isWinner();
+            isLoser();
+        }
     }
-    userGuess.push(event.key);
+};
 
 
-    console.log(userGuess);
+setup();
+updateScreen();
 
-
-
-
-}
-// if (animal.includes(userGuess)) {
-//     console.log(yes);
-// } else {
-//     console.log(no);
-// }
-// directionsText.textContent = "";
-// userChoiceText.textContent = "You chose: " + userGuess;
-// winsText.textContent = "Wins: " + wins;
-// lossesText.textContent = "Losses: " + losses;
-// tiesText.textContent = "Ties: " + ties;
-// } else {
-//     alert("Not a valid guess... try again!");
-// }
+console.log(ansWord);
